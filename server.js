5f14f32eb90129bfa1b489447ddf6c704843619e
Tatiana Grigorieva <tsg1204@gmail.com>
2020-04-02 11:28:08 -0400

    server change fro heroku deploy 8

--- server.js
+++ server.js
@@ -23,18 +23,18 @@ app.use(bodyParser.urlencoded({
   extended: true
 }))
 
-// if (process.env.NODE_ENV === 'production') {
-//     // Express will serve up production assets
-//     // like our main.js file, or main.css file!
-//     app.use(express.static('client/build'));
+if (process.env.NODE_ENV === 'production') {
+    // Express will serve up production assets
+    // like our main.js file, or main.css file!
+    app.use(express.static('client/build'));
   
-//     // Express will serve up the index.html file
-//     // if it doesn't recognize the route
-//     const path = require('path');
-//     app.get('*', (req, res) => {
-//       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//     });
-// }
+    // Express will serve up the index.html file
+    // if it doesn't recognize the route
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000"); 
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
